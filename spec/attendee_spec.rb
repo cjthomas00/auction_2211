a3ac5ef091781d317b8df99e85cec485d5000141
Caleb Thomas <cjthomas00@comcast.net>
2023-01-08 11:25:11 -0700

    Write test for #initialize.

---
+++ spec/attendee_spec.rb
@@ -0,0 +1,15 @@
+require './lib/attendee'
+
+RSpec.describe Attendee do
+  let(:attendee) { Attendee.new(name: 'Megan', budget: '$50') }
+
+  describe "#initialize" do 
+    it 'exists' do
+      expect(attendee).to be_instance_of(Attendee)
+    
+    it 'has attributes' do 
+      expect(attendee.name).to eq("Megan")
+      expect(attendee.budget).to eq('$50')
+    end
+  end
+end
