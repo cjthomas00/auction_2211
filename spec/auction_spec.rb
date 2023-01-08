7c40ba182bc57737e8ee2671d53d2afe6874f919
Caleb Thomas <cjthomas00@comcast.net>
2023-01-08 12:13:36 -0700

    write #unpopular_items test, and update Item to include bids hash.

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe Auction do
   let(:auction) { Auction.new }
   let(:item1) { Item.new('Chalkware Piggy Bank') }
   let(:item2) { Item.new('Bamboo Picture Frame') }
+  let(:item3) { Item.new('Homemade Chocolate Chip Cookies') }
+  let(:item4) { Item.new('2 Days Dogsitting') }
+  let(:item5) { Item.new('Forever Stamps') }
+  let(:attendee1) { Attendee.new(name: 'Megan', budget: '$50') }
+  let(:attendee) { Attendee.new(name: 'Bob', budget: '$75') }
+  let(:attendee3) { Attendee.new(name: 'Mike', budget: '$100') }
 
   describe "#initialize" do 
     it 'exists' do
@@ -32,6 +38,18 @@ RSpec.describe Auction do
       auction.add_item(item2)
     
       expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])  
+    end
+  end
+
+  describe "#unpopular_items" do
+    it 'returns an array of unpopular items' do 
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      expect(item1.bids).to eq({})
     end
   end
 end
