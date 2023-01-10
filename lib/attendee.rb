1358fe32f547edf1f4ad97aa56583e07c19c2ae5
Caleb Thomas <cjthomas00@comcast.net>
2023-01-10 10:21:34 -0700

    add notes to file.

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -3,6 +3,6 @@ class Attendee
 
   def initialize(info)
     @name = info[:name]
-    @budget = info[:budget].delete_prefix('$').to_i
+    @budget = info[:budget].delete_prefix('$').to_i #< ==== Converts '$50' to 50.
   end
 end
